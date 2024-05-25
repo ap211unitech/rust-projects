9974d76430f1206fbb8b472615aba76bf9122d56
Arjun Porwal <65214523+ap211unitech@users.noreply.github.com>
2024-05-25 15:15:05 +0530

    chore: minor change

--- src/json_handler.rs
+++ src/json_handler.rs
@@ -47,7 +47,6 @@ pub fn write_json() -> Result<(), Box<dyn Error>> {
         name: String::from("Arjun Porwal"),
         is_famous: true,
     };
-
     let json = serde_json::to_string(&author).unwrap();
     println!("{}", json);
     Ok(())
